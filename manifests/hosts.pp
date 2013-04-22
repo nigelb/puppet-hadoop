c0d49fd3c0cd3d358d4f828c3128b1b669ce6b94
nigelb <nigel.blair@gmail.com>
2013-04-22 13:35:41 +1000

    Update hosts manifest

--- manifests/hosts.pp
+++ manifests/hosts.pp
@@ -1,11 +1,20 @@
 class hadoop::hosts
 {
-        define add_host($fqdn, $ip, $host_aliases)
+        define add_host($fqdn, $ip, $host_aliases = "UNSET")
         {
-                host{$fqdn:
-                        ip => $ip,
-                        host_aliases => $host_aliases
-                }
+		if $host_aliases == "UNSET"
+		{
+	                host{$fqdn:
+        	                ip => $ip,
+	                }
+
+		}else
+		{
+	                host{$fqdn:
+        	                ip => $ip,
+                	        host_aliases => $host_aliases
+	                }
+		}
         }
         create_resources(hadoop::hosts::add_host, $::hosts)
 }
