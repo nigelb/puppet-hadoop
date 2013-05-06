3ef8eb4341ba057d40c00309b81944538e9c7bf0
nigelb <nigel.blair@gmail.com>
2013-05-07 08:34:31 +1000

    Add service setup

---
+++ manifests/cluster/service.pp
@@ -0,0 +1,22 @@
+
+
+hadoop::cluster::service{
+
+	file{"${hadoop::params::hadoop_base}/hadoop-${hadoop::params::version}/bin/ssh_initilize.sh":
+		alias   => "ssh-init-script"
+		content => template("hadoop/bin/ssh_initilize.sh.erb"),
+		require => File["hadoop-master"],
+		mode    => 744
+	}
+
+	Exec{ path => ["/bin", "/usr/bin", "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::version}/bin"] }
+
+	exec{"${hadoop::params::hadoop_base}/hadoop-${hadoop::params::version}/bin/ssh_initilize.sh":
+		alias   => "ssh-init"
+		command => "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::version}/bin/ssh_initilize.sh"
+		require => File["ssh-init-script"],
+		user    => $hadoop::params::hadoop_user,
+		group   => $hadoop::params::hadoop_group,
+	}
+
+}
